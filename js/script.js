afdbabc337cf96b87ab33110b71c6d18f5e909a1
mrgreenery <wilbergroen@gmail.com>
2024-11-15 09:31:20 +0100

    Watering can moves

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 //MAKE THE MAGIC HAPPEN
+
+$(document).ready(function() { // Add event listeners for mouse enter and mouse leave 
+    $("#wateringcan").hover( 
+        function() { 
+            $(this).addClass("tilt"); 
+        }, 
+        function() { 
+            $(this).removeClass("tilt"); 
+        } 
+    ); 
+});
