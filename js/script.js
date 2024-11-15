0ecc3cd9d49c46713bea4cb9482c45d1017bdb64
BbyBruh <reiashtray@gmail.com>
2024-11-15 10:35:35 +0100

    first com

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 //MAKE THE MAGIC HAPPEN
 
-$(document).ready(function() { // Add event listeners for mouse enter and mouse leave 
-    $("#wateringcan").hover( 
-        function() { 
-            $(this).addClass("tilt"); 
-        }, 
-        function() { 
-            $(this).removeClass("tilt"); 
-        } 
-    ); 
+$(document).ready(function () { // Add event listeners for mouse enter and mouse leave 
+    $("#wateringcan").hover(
+        function () {
+            $(this).addClass("tilt");
+        },
+        function () {
+            $(this).removeClass("tilt");
+        }
+    );
+});
+
+
+$(".apple").hover(function () {
+    alert("You entered p1!");
+},
+    function () {
+        alert("Bye! You now leave p1!");
+    });
+
+$(".apple").click(function () {
+    $(this).hide();
 });
