2241781c1b63f4b6176a0a938e2f78d80b5949ad
Samojak <samo.jakabovic11@gmail.com>
2024-11-15 10:27:31 +0100

    net

--- js/script.js
+++ js/script.js
@@ -1,12 +1,61 @@
 //MAKE THE MAGIC HAPPEN
 
-$(document).ready(function() { // Add event listeners for mouse enter and mouse leave 
-    $("#wateringcan").hover( 
-        function() { 
-            $(this).addClass("tilt"); 
-        }, 
-        function() { 
-            $(this).removeClass("tilt"); 
-        } 
-    ); 
+$(document).ready(function () { // Add event listeners for mouse enter and mouse leave 
+    $("#wateringcan").hover(
+        function () {
+            $(this).addClass("tilt");
+        },
+        function () {
+            $(this).removeClass("tilt");
+        }
+    );
 });
+
+
+
+let previousX = 0;
+let flipped;
+$(document).mousemove(function (e) {
+    const currentX = e.pageX;
+
+
+
+    if (currentX > previousX) {
+        $("#net").css("transform", "scaleX(-1)");
+        flipped = true;
+
+    } else {
+        $("#net").css("transform", "scaleX(1)");
+        flipped = false;
+    }
+
+
+    $("#net").css({
+        left: e.pageX - 60,
+        top: e.pageY - 40
+    });
+
+
+    previousX = currentX;
+});
+
+$("#net").click(function () {
+
+    if (flipped === false) {
+        $(this).addClass("rotate");
+
+
+        setTimeout(() => {
+            $(this).removeClass("rotate");
+        }, 1000);
+    }
+    if (flipped === true) {
+        $(this).addClass("rotateFlipped");
+
+
+        setTimeout(() => {
+            $(this).removeClass("rotateFlipped");
+        }, 1000);
+    }
+});
+
