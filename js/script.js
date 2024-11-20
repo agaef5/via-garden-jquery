23f19c2d823fdf513205bd21905e3c47363225b4
Aga Furgalska <aga.furgalska@wp.pl>
2024-11-20 12:04:09 +0100

    Squashed commit of the following:
    commit 23a422d4fc16b7ea86fd06e0df805a7ad603bc6f
    Author: mrgreenery <wilbergroen@gmail.com>
    Date:   Fri Nov 15 14:46:26 2024 +0100
    
        good enuff
    
    commit d8543d8cc598fdb50dec1617c8b67b522f896606
    Author: mrgreenery <wilbergroen@gmail.com>
    Date:   Fri Nov 15 14:06:04 2024 +0100
    
        working watering can
    
    commit b306818c6ff4c75e2e197fc13025c3e38ac6a607
    Author: mrgreenery <wilbergroen@gmail.com>
    Date:   Fri Nov 15 10:35:23 2024 +0100
    
        watering can

--- js/script.js
+++ js/script.js
@@ -260,3 +260,105 @@ $(document).ready(function () {
 });
 
 //Ash
+$(document).ready(function () {
+  const $wateringCan = $("#wateringcan img"); // Select the image inside the watering can div
+  const $waterdrops = $(".waterdrop");
+  let animationIntervals = [];
+  const canColors = [
+    "images/wateringcan.png",
+    "images/wateringcanblue.png",
+    "images/wateringcangreen.png",
+    "images/wateringcanyellow.png",
+  ];
+  let currentColorIndex = 0;
+
+  // Start with hiding all waterdrops
+  $waterdrops.hide();
+
+  $wateringCan.click(function () {
+    $(this).parent().toggleClass("tilt");
+
+    if ($(this).parent().hasClass("tilt")) {
+      // Add a small delay before starting the water animation
+      setTimeout(startWaterAnimation, 500);
+    } else {
+      stopWaterAnimation();
+      changeCanColor(); // Change color when untitled
+    }
+  });
+
+  function startWaterAnimation() {
+    // Ensure the can is still tilted before starting
+    if (!$wateringCan.parent().hasClass("tilt")) return;
+
+    // Reset all drops before starting new animation
+    stopWaterAnimation();
+
+    $waterdrops.each(function (index) {
+      const $drop = $(this);
+      animationIntervals[index] = setTimeout(function () {
+        animateWaterdrop($drop);
+      }, index * 500); // Stagger start times
+    });
+  }
+
+  function stopWaterAnimation() {
+    // Clear all timeouts
+    animationIntervals.forEach(clearTimeout);
+    animationIntervals = [];
+
+    // Stop all animations and hide drops immediately
+    $waterdrops.stop(true, true).hide();
+  }
+
+  function animateWaterdrop($drop) {
+    if (!$wateringCan.parent().hasClass("tilt")) {
+      $drop.hide();
+      return;
+    }
+
+    const canRect = $wateringCan[0].getBoundingClientRect();
+
+    const spoutOffsetFromRight = canRect.width * 0.9;
+    const spoutOffsetFromTop = canRect.height * 0.48;
+
+    // Add randomness to the starting position
+    const randomOffsetX = Math.random() * 10 - 5;
+    const randomOffsetY = Math.random() * 10 - 5;
+
+    const spoutX = canRect.right - spoutOffsetFromRight + randomOffsetX;
+    const spoutY = canRect.top + spoutOffsetFromTop + randomOffsetY;
+
+    const endPosition = $(window).height() + 50;
+    const duration = 2000 + Math.random() * 500;
+
+    $drop
+      .css({
+        left: spoutX,
+        top: spoutY,
+        display: "block",
+      })
+      .animate(
+        {
+          top: endPosition,
+        },
+        {
+          duration: duration,
+          easing: "linear",
+          complete: function () {
+            if ($wateringCan.parent().hasClass("tilt")) {
+              animateWaterdrop($drop);
+            } else {
+              $drop.hide();
+            }
+          },
+        }
+      );
+  }
+
+  function changeCanColor() {
+    // Change to the next color image
+    currentColorIndex = (currentColorIndex + 1) % canColors.length;
+    $wateringCan.attr("src", canColors[currentColorIndex]); // Change image source
+  }
+});
